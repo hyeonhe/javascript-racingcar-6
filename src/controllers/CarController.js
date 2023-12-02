69d08e0b991c6899106921afde2c08a74072021f
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-12-02 19:49:19 +0900

    refactor: CarController 메서드 분리

--- src/controllers/CarController.js
+++ src/controllers/CarController.js
@@ -3,23 +3,28 @@ import Cars from "../models/Cars";
 import Count from "../models/Count";
 import generateRandomNumber from "../utils/generateRandomNumber";
 import InputView from "../views/InputView";
-import OutputView from "../views/outputView";
+import OutputView from "../views/OutputView";
 
 class CarController {
   async init() {
-    const carNamesInput = await InputView.carNames();
-    const cars = new Cars(carNamesInput);
-    const carNames = cars.getCarNames();
-    console.log(carNames);
-
-    const inputCount = await InputView.count();
-    const count = new Count(inputCount);
-    const counts = count.getCount();
-    console.log(counts);
+    const carNames = await this.getCarNames();
+    const counts = await this.getCount();
 
     const carForwards = new Array(carNames.length).fill(0);
     OutputView.printResult();
     this.playRounds(counts, carNames, carForwards);
+  }
+
+  async getCarNames() {
+    const carNamesInput = await InputView.carNames();
+    const cars = new Cars(carNamesInput);
+    return cars.getCarNames();
+  }
+
+  async getCount() {
+    const inputCount = await InputView.count();
+    const count = new Count(inputCount);
+    return count.getCount();
   }
 
   playRounds(counts, carNames, carForwards) {
